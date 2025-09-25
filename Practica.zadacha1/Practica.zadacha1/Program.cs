84f8ea2922289f7bb5c85c810a26e142f30eb47f
st56 <zahark456@gmail.com>
2025-09-25 09:38:05 +0300

    неправильное решение, выводит только каким числом является число четным или нечетным

--- Practica.zadacha1/Practica.zadacha1/Program.cs
+++ Practica.zadacha1/Practica.zadacha1/Program.cs
@@ -20,6 +20,37 @@ namespace Practica.zadacha1
             {
                 array[i] = Convert.ToInt32(Console.ReadLine());
             }
+            /*
+            if (array[i]%2==0)
+            {
+                array[i] = 1;
+                int sum1 = 0;
+                int sum = 0;
+                sum1 = (array[i] + 1;
+                sum = n - sum1;
+                Console.WriteLine("Количество четных чисел:   " + sum1);
+                Console.WriteLine("Количество нечетных чисел: " + sum);
+            }
+            */
+            for (int i = 0; i < array.Length; i++)
+            {
+                int sum1 = 0;
+                int sum = 0;
+                if (array[i] % 2 == 0)
+                {
+                    Console.WriteLine("Четные числа:   " + array[i]);
+                    array[i] = 1;
+                    sum1 = array[i];
+                    Console.WriteLine("Количество четных чисел:   " + sum1);
+                }
+                else
+                {
+                    Console.WriteLine("Нечетные числа: " + array[i]);
+                    array[i] = 1;
+                    sum = n - sum1;
+                    Console.WriteLine("Количество нечетных чисел: " + sum);
+                }
+            }
         }
     }
 }
