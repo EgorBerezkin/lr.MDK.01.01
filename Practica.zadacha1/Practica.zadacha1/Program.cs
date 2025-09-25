d60ac8f700f84c20c9df2a6668145732b1fa65c6
st56 <zahark456@gmail.com>
2025-09-25 08:55:13 +0300

    Создали массив, в котором с клавиатуры вводится количество и сами элементы массива

--- Practica.zadacha1/Practica.zadacha1/Program.cs
+++ Practica.zadacha1/Practica.zadacha1/Program.cs
@@ -10,6 +10,16 @@ namespace Practica.zadacha1
     {
         static void Main(string[] args)
         {
+            // Вариант 5
+            // Задание 1
+            Console.WriteLine("Введите количество чисел в массиве: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            Console.WriteLine("Введите элементы массива: ");
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
         }
     }
 }
