9746c89cf86fc8a7841b951b0922f1d8ef10927f
Egor Briggs <berezkin07egor@gmail.com>
2025-10-10 22:31:01 +0300

    Создал цикл который складывает первое число с третим, третрье с пятым и т.д., в конце выводит суммы этих чисел

--- LR_1/Zadacha2/Program.cs
+++ LR_1/Zadacha2/Program.cs
@@ -10,13 +10,22 @@ namespace Zadacha2
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[3000];
+            int[] array = new int[3000];
             Random random = new Random();
-            for (int i = 0; i < numbers.Length; i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                numbers[i] = random.Next(10, 10000);
+                array[i] = random.Next(10, 10000);
             }
-
+            Console.WriteLine("Исходные данные: ");
+            Console.WriteLine("[" + string.Join(", ", array) + "]");
+            List<int> Summa = new List<int>();
+            for (int i = 0;i < array.Length - 2;i+=2)
+            {
+                int sum = array[i] + array[i+2];
+                Summa.Add(sum);
+            }
+            Console.WriteLine("Результат: ");
+            Console.WriteLine("[" + string.Join(", ", Summa) + "]");
         }
     }
 }
