d7e60d29693d4b9e1e0e5c2d7f04cbcf1db136b2
Egor Briggs <berezkin07egor@gmail.com>
2025-10-15 21:12:54 +0300

    Создал новый класс Поисковый модуль Доделал запрос пользователя, а также написал код о товаре определённой категориии цена товара определённой категории

---
+++ LR_2/Zadacha_1/Zadacha_1/SearchingModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha_1
+{
+    public class SearchingModule
+    {
+        public int FindIndexCategory(string userQuery, string[] categories)
+        {
+            for(int index = 0; index < categories.Length; ++ index)
+            {
+                string priceCategory = categories[index];
+                if(priceCategory == userQuery)
+                {
+                    return index;
+                }
+            }
+            return -1;
+        }
+        public (List<string>, List<double>) FindAllProductsByCategory(int IndexCategory, List<string>[] AllProducts, List<double>[] price)
+        {
+            List<string> productsByCategory = new List<string>();
+            List<double> priceByCategory = new List<double>();
+            productsByCategory = AllProducts[IndexCategory];
+            priceByCategory = price[IndexCategory];
+            return (productsByCategory, priceByCategory);
+        }
+
+    }
+}
