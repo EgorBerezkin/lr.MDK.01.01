19949df57b833812041f0b0c7be2a6765c91540a
Egor Briggs <berezkin07egor@gmail.com>
2025-10-17 20:59:09 +0300

    Создал перечисление, также ввел переменную которую вводит пользователь, если число которое ввел пользователь от 1 до 7 то тогда нам выводит день неделю, а если неверно то просит пользователя ввести цифру

--- DZ/Perechislenie/Program.cs
+++ DZ/Perechislenie/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,37 @@ namespace Perechislenie
 {
     internal class Program
     {
+        enum DayOfWeek
+        {
+            Понедельник = 1,
+            Вторник,
+            Среда,
+            Четверг,
+            Пятница,
+            Суббота,
+            Воскресенье
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите номер дня недели (1-7): ");
+            int DayWeek;
+            if (int.TryParse(Console.ReadLine(), out DayWeek))
+            {
+                if (DayWeek >= 1 && DayWeek <= 7)
+                {
+                    DayOfWeek day = (DayOfWeek)DayWeek;
+                    Console.WriteLine($"Это {day}");
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный номер. Введите число от 1 до 7.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный номер. Введите число от 1 до 7.");
+            }
+            Console.ReadLine();
         }
     }
 }
