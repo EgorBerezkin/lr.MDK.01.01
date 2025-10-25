522ee4e04361e1ff3ce9029217c87f39cc454af8
Fa1tloy <baranovdaniil534@gmail.com>
2025-10-25 12:20:53 +0300

    Создал словарь Clothes, а также добавил  три ключа сосвоими значениями

--- Dictionary/Dict/Program.cs
+++ Dictionary/Dict/Program.cs
@@ -10,6 +10,14 @@ namespace Dict
     {
         static void Main(string[] args)
         {
+            Dictionary<string, List<string>> Clothes = new Dictionary<string, List<string>> ();
+            string hats = "Головные уборы";
+            Words.Add(hats, new List<string>() {"Шляпа", "Бейсболка", "Шапка" });
+            string waistwear = "Поясная одежда";
+            Words.Add(waistwear, new List<string>() { "Джинсы", "Штаны", "Юбка" });
+            string shoulder_wear = "Плечевая одежда";
+            Words.Add(shoulder_wear, new List<string>() { "Свитер", "Топик", "Футболка" });
+
         }
     }
 }
