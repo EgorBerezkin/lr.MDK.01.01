ed738ab6fc59814f591392a7d5d98ec8706f360b
Egor Briggs <berezkin07egor@gmail.com>
2025-11-10 20:49:25 +0300

    Создал программу для проекта по покупке вещей для образа, который содержит три характеристики. Во всем коде написано подсказки для объяснения самого кода

---
+++ DZ/Data_structure/ShoppingProject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_structure
+{
+    public class ShoppingProject
+    {
+        public string Name { get; set; }    // get метод для получения значения свойства
+                                            // set метод для установки значения свойства
+        public int ItemCount { get; set; }
+        public int ShoppingTime { get; set; }
+        public decimal Budget { get; set; }
+        public ShoppingProject(string name, int itemCount, int shoppingTime, decimal budget)
+        {
+            Name = name;
+            ItemCount = itemCount;
+            ShoppingTime = shoppingTime;
+            Budget = budget;
+        }
+    
+        public override string ToString() // Объявление метода
+        {
+            // Возвращает строку с форматированными данными объекта
+            return $"{Name} | Предметов: {ItemCount},  Время: {ShoppingTime} мин,  Бюджет: {Budget} руб";
+        }
+    }
+}
