d99d567b2ff99f55b0b7d4b8dcf2758157e8ed5a
Fa1tloy <baranovdaniil534@gmail.com>
2025-11-25 13:24:10 +0300

    Создал классы

--- ManyClassesAplication/Program.cs
+++ ManyClassesAplication/Program.cs
@@ -10,7 +10,19 @@ namespace ManyClassesAplication
     {
         static void Main(string[] args)
         {
+            Product Mers = new Product();
+            Mers.SetName("Mersedez");
+            Mers.SetPrice(10000000);
 
+            Product VAS = new Product();
+            VAS.SetName("VAS");
+            VAS.SetPrice(1500000);
+
+            Warehouse Avto = new Warehouse();
+            Avto.SetIdentifier(123);
+            Avto.SetLocate("Торжок, ул. Студенческая, д. 3");
+            Avto.SetProductCount(Mers, 2);
+            Avto.SetProductCount(VAS, 5);
         }
     }
 }
