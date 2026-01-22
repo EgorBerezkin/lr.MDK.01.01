8c6dde8b303511de3ba816e9794f80832fff3421
Egor Briggs <berezkin07egor@gmail.com>
2026-01-22 20:17:46 +0300

    Перекинул проект в свой репозиторий

---
+++ TextUsing/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace TextUsing
+{
+    partial class FormCheck
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DatePickerTime = new System.Windows.Forms.DateTimePicker();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ButtonCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DatePickerTime
+            // 
+            this.DatePickerTime.CalendarMonthBackground = System.Drawing.Color.LightPink;
+            this.DatePickerTime.CalendarTrailingForeColor = System.Drawing.Color.LightPink;
+            this.DatePickerTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.DatePickerTime.Location = new System.Drawing.Point(62, 97);
+            this.DatePickerTime.Name = "DatePickerTime";
+            this.DatePickerTime.Size = new System.Drawing.Size(139, 20);
+            this.DatePickerTime.TabIndex = 3;
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.BackColor = System.Drawing.Color.LightPink;
+            this.InputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.InputTextBox.Location = new System.Drawing.Point(272, 139);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(157, 29);
+            this.InputTextBox.TabIndex = 5;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.LightPink;
+            this.richTextBox1.Location = new System.Drawing.Point(16, 186);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(413, 70);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Угадай число";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(207, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = " - обратный отсчет";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(254, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Введите число от 1 до 100:";
+            // 
+            // ButtonCheck
+            // 
+            this.ButtonCheck.BackColor = System.Drawing.Color.LightPink;
+            this.ButtonCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonCheck.Location = new System.Drawing.Point(272, 12);
+            this.ButtonCheck.Name = "ButtonCheck";
+            this.ButtonCheck.Size = new System.Drawing.Size(157, 57);
+            this.ButtonCheck.TabIndex = 10;
+            this.ButtonCheck.Text = "Проверь";
+            this.ButtonCheck.UseVisualStyleBackColor = false;
+            this.ButtonCheck.Click += new System.EventHandler(this.ButtonCheck_Click);
+            // 
+            // FormCheck
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(448, 268);
+            this.Controls.Add(this.ButtonCheck);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.DatePickerTime);
+            this.Name = "FormCheck";
+            this.Text = "Проверка числа";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DateTimePicker DatePickerTime;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ButtonCheck;
+    }
+}
+
---
+++ TextUsing/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextUsing
+{
+    public partial class FormCheck : Form
+    {
+        int unknownNumber_;
+        public FormCheck()
+        {
+            InitializeComponent();
+
+            Random random = new Random();
+            unknownNumber_ = random.Next(1, 101);
+        }
+
+        private void ButtonCheck_Click(object sender, EventArgs e)
+        {
+           string input = InputTextBox.Text;
+            int num = Convert.ToInt32(input);
+
+            if (unknownNumber_ == num)
+            {
+                MessageBox.Show("Число угадано");
+            }
+            else if (unknownNumber_ > num)
+            {
+                MessageBox.Show("Число больше загадонного");
+            }
+            else 
+            {
+                MessageBox.Show("Число меньше загаданного");
+            }
+        }
+    }
+}
