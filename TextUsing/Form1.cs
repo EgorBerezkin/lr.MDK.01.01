85280d4db83e22d2e8c0acfec320f1a654b25479
Egor Briggs <berezkin07egor@gmail.com>
2026-01-22 21:56:55 +0300

    Добавил обратный отчет

--- TextUsing/Form1.cs
+++ TextUsing/Form1.cs
@@ -13,22 +13,57 @@ namespace TextUsing
     public partial class FormCheck : Form
     {
         int unknownNumber_;
+        int secondsLeft = 60;
+        bool gameFinished = false;
+        System.Windows.Forms.Timer timer;
+
         public FormCheck()
         {
             InitializeComponent();
 
             Random random = new Random();
             unknownNumber_ = random.Next(1, 101);
+
+            // Настраиваем DateTimePicker
+            DatePickerTime.Format = DateTimePickerFormat.Time;
+            DatePickerTime.ShowUpDown = true;
+            DatePickerTime.Value = new DateTime(1, 1, 1, 0, 1, 0);
+            DatePickerTime.Enabled = false;
+
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            secondsLeft--;
+
+            // Не даем secondsLeft стать отрицательным
+            if (secondsLeft < 0) secondsLeft = 0;
+
+            // Обновляем DateTimePicker (минимально 0 секунд)
+            DatePickerTime.Value = new DateTime(1, 1, 1, 0, 0, Math.Max(0, secondsLeft));
+
+            if (secondsLeft <= 0 && !gameFinished)
+            {
+                timer.Stop();
+                gameFinished = true;
+                MessageBox.Show($"Время вышло! Число: {unknownNumber_}");
+            }
         }
 
         private void ButtonCheck_Click(object sender, EventArgs e)
         {
-           string input = InputTextBox.Text;
+            if (secondsLeft <= 0) return;
+
+            string input = InputTextBox.Text;
             int num = Convert.ToInt32(input);
 
             if (unknownNumber_ == num)
             {
-                MessageBox.Show("Число угадано");
+                MessageBox.Show($"Число угадано! Осталось времени: {secondsLeft} сек");
             }
             else if (unknownNumber_ > num)
             {
@@ -38,6 +73,11 @@ namespace TextUsing
             {
                 MessageBox.Show("Число меньше загаданного");
             }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
