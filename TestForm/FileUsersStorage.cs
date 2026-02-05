68d7986a64108c2ca429e276d0ed1f0cbb7df129
Egor Briggs <berezkin07egor@gmail.com>
2026-02-05 17:56:08 +0300

    перекинул проект

---
+++ TestForm/FileUsersStorage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestForm
+{
+    public class FileUsersStorage : IUsersInterface
+    {
+        public List<User> Load()
+        {
+            List<User> allers = new List<User>();
+            string patch = "LoginAndPassword.txt";
+            StreamReader reader = new StreamReader(patch);
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] lines = line.Split('-');
+                allers.Add(new User(lines[0], lines[1]));
+            }
+            return allers;
+        }
+
+    }
+}
---
+++ TestForm/MainForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestForm
+{
+    public partial class MainForm : Form
+    {
+        private FileUsersStorage AllLoginAndPassword = new FileUsersStorage();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            List<User> check = AllLoginAndPassword.Load();
+            string login = LoginСomboBox.Text;
+            string password = PasswordTextBox.Text;
+            User user = new User(login, password);
+            if(Contains(check, user))
+            {
+                MessageBox.Show("Уроооо, ты смог зайти!!!");
+            }
+            else
+            {
+                MessageBox.Show("Оуууу ноу, не взламывай");
+            }
+
+        }
+        private bool Contains(List<User> check, User user)
+        {
+            foreach (User u in check)
+            {
+                if (user.Login == u.Login && user.Password == u.Password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void LoginСomboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedUser = LoginСomboBox.SelectedItem.ToString();
+            List<User> userSelected = 
+        }
+    }
+}
