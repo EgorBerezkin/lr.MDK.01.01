a5285312b4f0004c4c82e192add3fbc58b812031
Fa1tloy <baranovdaniil534@gmail.com>
2026-03-28 08:29:29 +0300

    удалил не полный проект и добавил правильный

--- TestFileStorage/TestFileStorage/Program.cs
+++ TestFileStorage/TestFileStorage/Program.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace TestFileStorage
 {
-    internal static class Program
+    static class Program
     {
         /// <summary>
         /// Главная точка входа для приложения.
